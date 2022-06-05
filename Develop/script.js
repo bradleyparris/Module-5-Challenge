404021f7c0cc2cbc0ae897f10560d92f79925116
Bradley Parris <parrisbjp@gmail.com>
2022-06-05 19:56:37 -0400

    color codes time blocks

--- Develop/script.js
+++ Develop/script.js
@@ -2,3 +2,20 @@
 var currentDay = moment().format('dddd MMM Do');
 $("#currentDay").html(currentDay);
 
+// Determine if time blocks are past, present, or future
+var currentHour = moment().hour();
+console.log(currentHour);
+$(".time-block").each(function() {
+    var blockTime = $(this).attr("id");
+    console.log(blockTime);
+
+    if (blockTime == currentHour) {
+        $(this).addClass("present");
+    }
+    else if (blockTime < currentHour) {
+        $(this).addClass("past");
+    }
+    else if (blockTime > currentHour) {
+        $(this).addClass("future");
+    }
+})
