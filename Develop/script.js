c9abfd4bab5974742a64d951ad130794421f9723
Bradley Parris <parrisbjp@gmail.com>
2022-06-05 20:33:27 -0400

    buttons save to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -19,3 +19,21 @@ $(".time-block").each(function() {
         $(this).addClass("future");
     }
 })
+
+// Save buttons
+$(".saveBtn").on("click", function() {
+    var time = $(this).parent().attr("id");
+    var text = $(this).siblings(".description").val();
+    localStorage.setItem(time, text);
+})
+
+// Pulls from local storage
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
